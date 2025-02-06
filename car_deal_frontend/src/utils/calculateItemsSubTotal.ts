e538e171f34cf2edf445e4eb093168a9a9cd8218
CK-EmbPro <kennydebrice2@gmail.com>
2025-02-06 17:02:01 +0200

    Updated: car_deal_frontend/src/utils/calculateItemsSubTotal.ts

--- car_deal_frontend/src/utils/calculateItemsSubTotal.ts
+++ car_deal_frontend/src/utils/calculateItemsSubTotal.ts
@@ -1,5 +1,6 @@
-import { CartItem } from "@/types/cart"
+import { ISavedCartItem } from "@/types/cart"
 
-export const calculateItemsSubtotal = (cartItems: CartItem[]) => {
-    return cartItems.reduce((total, item) => total + (item.price * item.itemQuantity), 0)
+export const calculateItemsSubtotal = (cartItems:ISavedCartItem[]) => {
+    if(cartItems.length === 0) return 0
+    return cartItems.reduce((total, item) => total + (item.unitPrice * item.quantity), 0)
 }
