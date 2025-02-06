6d16f17e8fcdf04f3882d7a2d621dc2ca20b7f60
CK-EmbPro <kennydebrice2@gmail.com>
2025-02-06 17:02:01 +0200

    Updated: car_deal_backend/types/order.type.ts

--- car_deal_backend/types/order.type.ts
+++ car_deal_backend/types/order.type.ts
@@ -1,13 +1,14 @@
 import { ORDER_STATUS } from "../constants/orderStatus";
 
 export interface IOrder {
+  _id: string
   carName: string;
   totalPrice: number;
   carImageName: string;
   carImageCloudId: string;
   carImageCloudUrl: string;
   carQuantity: number;
-  orderStatus: (typeof ORDER_STATUS)[keyof typeof ORDER_STATUS];
+  orderStatus: ( typeof ORDER_STATUS)[keyof typeof ORDER_STATUS];
   userEmail: string;
   userPhoneNumber: string;
 }
