cf330e951288c476e6b0559dfb465217d5999655
CK-EmbPro <kennydebrice2@gmail.com>
2025-02-06 17:02:01 +0200

    Updated: car_deal_frontend/src/constants/common.ts

--- car_deal_frontend/src/constants/common.ts
+++ car_deal_frontend/src/constants/common.ts
@@ -1,2 +1,7 @@
 export const USER_COOKIE= 'auth_user'
 export const TOKEN_COOKIE = 'token'
+export enum ORDER_STATUS {
+    PENDING= "PENDING",
+    APPROVED= "APPROVED",
+    REJECTED= "REJECTED",
+}
