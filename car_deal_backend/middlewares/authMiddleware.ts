01e6bff4f7954fb56734bc27ae24ba97316ddc3c
CK-EmbPro <kennydebrice2@gmail.com>
2025-02-12 08:51:50 +0200

    Updated: car_deal_backend/middlewares/authMiddleware.ts

--- car_deal_backend/middlewares/authMiddleware.ts
+++ car_deal_backend/middlewares/authMiddleware.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
 import { verifyToken } from "../utils/jwt";
 import { UserModel } from "../models/User";
 import { BadRequestError, NotFoundError, UnauthorizedError } from "../exceptions/errors";
 import mongoose from "mongoose";
 import { ApiResponse } from "../apiResponse/ApiResponse";
+import { AUTH_COOKIE_NAME } from "../constants/common";
 
 export const authMiddleware = async (
     req: Request,
@@ -13,12 +13,11 @@ export const authMiddleware = async (
     next: NextFunction
 ) => {
     try {
-        const token = req.header("Authorization")?.substring(7);
+        const token = req.cookies[AUTH_COOKIE_NAME];
 
+        console.log('token ', req.cookies);
         if (!token) {
-            return res.status(401).json({
-                message: "Auth Token required",
-            });
+            throw new UnauthorizedError("Please login first")
         }
 
         // Decode the token
@@ -33,6 +32,12 @@ export const authMiddleware = async (
             throw new BadRequestError("Invalid token, user not found")
         }
 
+        const {password, ...rest} = currentUser.toObject()
+        console.log('rest ', rest);
+        // Pass user info in request
+        req.user=rest
+
+        console.log('rest ', rest);
         // If user is valid continue with request
         next();
 
@@ -41,8 +46,12 @@ export const authMiddleware = async (
         if (error instanceof BadRequestError) {
             return res.status(400).json(new ApiResponse(error.message, null))
         } else if (error instanceof jwt.TokenExpiredError) {
-            return res.status(400).json(new ApiResponse(error.message, null));
+            res.clearCookie(AUTH_COOKIE_NAME)
+            req.user= null
+            return res.status(400).json(new ApiResponse(`${error.message}, Please login again`, null));
         } else if (error instanceof jwt.JsonWebTokenError) {
+            res.clearCookie(AUTH_COOKIE_NAME)
+            req.user=null
             return res.status(400).json(new ApiResponse(error.message, null));
         } else if (error instanceof NotFoundError) {
             return res.status(404).json(new ApiResponse(error.message, null));
