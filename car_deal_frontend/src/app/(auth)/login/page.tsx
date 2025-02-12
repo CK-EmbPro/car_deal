5cd8ec4ddc6769fac1825c2e4988e83f884cf5a0
CK-EmbPro <kennydebrice2@gmail.com>
2025-02-12 08:51:50 +0200

    Updated: car_deal_frontend/src/app/(auth)/login/page.tsx

--- car_deal_frontend/src/app/(auth)/login/page.tsx
+++ car_deal_frontend/src/app/(auth)/login/page.tsx
@@ -1,11 +1,13 @@
 "use client"
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import SignUpBanner from "../../../../public/signup/signup_login_banner.svg"
 import "react-phone-input-2/lib/style.css"
 import { useMutation } from '@tanstack/react-query'
 import { loginApi } from '@/api/auth/authApis'
 import { ILoginData } from '@/types/user'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { CERTAIN_ERROR, FORBIDDEN, JWT_EXPIRED, NOT_LOGGED_IN } from '@/constants/common'
+import { NOTIMP, NOTINITIALIZED } from 'dns/promises'
 
 
 const Login = () => {
@@ -14,8 +16,8 @@ const Login = () => {
         email: "",
         password: ""
     })
-
     const router = useRouter()
+    const searchParams = useSearchParams()
 
     const { mutate } = useMutation({
         mutationFn: async () => await loginApi(loginData), // Define your API call here
@@ -55,6 +57,29 @@ const Login = () => {
 
         
     }
+
+
+    useEffect(() => {
+        const forbidden = searchParams.get(FORBIDDEN)
+        const notLoggedIn = searchParams.get(NOT_LOGGED_IN)
+        const jwtExpired = searchParams.get(JWT_EXPIRED)
+        const certainError = searchParams.get(CERTAIN_ERROR)
+        let redirectTimeout: NodeJS.Timeout
+
+      
+            if (forbidden || notLoggedIn || jwtExpired || certainError) {
+                const message = forbidden || jwtExpired || certainError || notLoggedIn
+                alert(message)
+                redirectTimeout = setTimeout(() => {
+                    router.replace("/login")
+                }, 3000);
+            }
+       
+        return () => {
+            if (redirectTimeout) clearTimeout(redirectTimeout);
+        }
+    }, [searchParams, router])
+
 
 
 
