bda041dc04675ae9d96d07f9f73556859c0a12c4
CK-EmbPro <kennydebrice2@gmail.com>
2025-02-12 08:51:50 +0200

    Updated: car_deal_frontend/src/constants/common.ts

--- car_deal_frontend/src/constants/common.ts
+++ car_deal_frontend/src/constants/common.ts
@@ -1,7 +1,13 @@
-export const USER_COOKIE= 'auth_user'
-export const TOKEN_COOKIE = 'token'
+export const AUTH_TOKEN_NAME = 'auth_token'
+export const USER_STORAGE_KEY= 'current_user'
 export enum ORDER_STATUS {
     PENDING= "PENDING",
     APPROVED= "APPROVED",
     REJECTED= "REJECTED",
 }
+
+// Middleware search params
+export const NOT_LOGGED_IN = 'not_logged_in'
+export const FORBIDDEN = 'forbidden'
+export const JWT_EXPIRED = 'jwt_expired'
+export const CERTAIN_ERROR = 'certain_error'
