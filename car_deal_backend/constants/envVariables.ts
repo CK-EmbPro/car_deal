d474f150f0592e53a44b8df8e0a4479e0d4af198
CK-EmbPro <kennydebrice2@gmail.com>
2025-02-12 08:51:50 +0200

    Updated: car_deal_backend/constants/envVariables.ts

--- car_deal_backend/constants/envVariables.ts
+++ car_deal_backend/constants/envVariables.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const ADMIN_EMAIL= process.env.ADMIN_EMAIL
 export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD
 export const ADMIN_PROFILE_PHOTO_CLOUD_URL= process.env.ADMIN_PROFILE_PHOTO_CLOUD_URL
@@ -7,3 +10,5 @@ export const ADMIN_PHONENUMBER=process.env.ADMIN_PHONENUMBER
 export const FRONTEND_URL=process.env.FRONTEND_URL
 export const STRIPE_SECRET=process.env.STRIPE_SECRET
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET
+export const TOKEN_EXPIREATION_TIME= process.env.TOKEN_EXPIREATION_TIME
+export const JWT_SECRET = process.env.JWT_SECRET
